10fce1f6b206ca2844fa8da5f34a1f4949c3ec8e
Lucas Andres Lizama Monje <lucaslizama3@hotmail.com>
2016-11-19 19:40:21 -0300

    Prueba del metodo de validacion de parametros pasando

--- test/web/RegistroRequestTest.java
+++ test/web/RegistroRequestTest.java
@@ -5,12 +5,22 @@
  */
 package web;
 
+import db.Usuario;
+import ejb.AbstractFacade;
+import ejb.UsuarioFacade;
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static java.util.Collections.*;
+import java.util.List;
 
 /**
  *
@@ -42,13 +52,35 @@ public class RegistroRequestTest {
      */
     @Test
     public void testValidarParametros() throws Exception {
-        System.out.println("validarParametros");
-        RegistroRequest instance = null;
-        boolean expResult = false;
-        boolean result = instance.validarParametros();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        UsuarioFacade usuarioFacade = mock(UsuarioFacade.class);
+        List<String> nombres = new ArrayList<String>();
+        
+        nombres.add("rut");
+        nombres.add("dv");
+        nombres.add("nombre");
+        nombres.add("apellidoPat");
+        nombres.add("apellidoMat");
+        nombres.add("correo");
+        nombres.add("user");
+        nombres.add("pass");
+        nombres.add("repetirPass");
+        
+        when(request.getParameter("rut")).thenReturn("12345678");
+        when(request.getParameter("dv")).thenReturn("5");
+        when(request.getParameter("nombre")).thenReturn("Lucas");
+        when(request.getParameter("apellidoPat")).thenReturn("Lizama");
+        when(request.getParameter("apellidoMat")).thenReturn("Monje");
+        when(request.getParameter("correo")).thenReturn("lucaslizama3@hotmail.com");
+        when(request.getParameter("user")).thenReturn("lucaslizama3");
+        when(request.getParameter("pass")).thenReturn("lol1234");
+        when(request.getParameter("repetirPass")).thenReturn("lol1234");
+        when(request.getParameterNames()).thenReturn(enumeration(nombres));
+        when(usuarioFacade.findAll()).thenReturn(new ArrayList<Usuario>());
+        
+        RegistroRequest rr = new RegistroRequest(request, response, usuarioFacade);
+        assertTrue(rr.validarParametros());
     }
     
 }
