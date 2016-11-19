dabeb3aa3f77c825be9605b656f10b697ce5aea1
Lucas Andres Lizama Monje <lucaslizama3@hotmail.com>
2016-11-19 19:40:02 -0300

    Parametros para registro de un usuario validados

--- src/java/web/RegistroRequest.java
+++ src/java/web/RegistroRequest.java
@@ -1,11 +1,14 @@
 package web;
 
+import db.Usuario;
+import ejb.UsuarioFacade;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
@@ -19,11 +22,13 @@ import javax.servlet.http.HttpServletResponse;
 public class RegistroRequest {
 
     private HashMap<String,String> parameters;
+    private UsuarioFacade usuarioFacade;
     private HttpServletRequest request;
     private HttpServletResponse response;
     private RequestDispatcher rd;
     
-    public RegistroRequest(HttpServletRequest request, HttpServletResponse response) {
+    public RegistroRequest(HttpServletRequest request, HttpServletResponse response,UsuarioFacade usuarioFacade) {
+        this.usuarioFacade = usuarioFacade;
         this.request = request;
         this.response = response;
         rd = request.getRequestDispatcher("registro.jsp");
@@ -43,7 +48,12 @@ public class RegistroRequest {
             return false;
         if(!validarRut(parameters.get("rut"),parameters.get("dv").charAt(0))) 
             return false;
-        
+        if(!validarCorreo())
+            return false;
+        if(!validarExistenciaUser())
+            return false;
+        if(!validarPassword())
+            return false;
         return true;
     }
     
@@ -58,18 +68,21 @@ public class RegistroRequest {
     
     private boolean validarDigitoVerificador(String rut,Character dv) throws ServletException, IOException{
         int factor = 9;
-        int suma = 0;
+        Integer suma = 0;
         Integer sumaAlternada = 0;
+        char[] digitos;
         
-        for (int i = rut.length() - 1; i >= 0; i++) {
+        for (int i = rut.length() - 1; i >= 0; i--) {
             Character digito = rut.charAt(i);
             suma += Integer.parseInt(digito.toString()) * factor;
             factor = (factor - 1) < 4 ? 9 : (factor - 1); 
         }
+
+        digitos = suma.toString().toCharArray();
         
-        for(Character digito : Integer.toString(suma).toCharArray()) {
-            sumaAlternada += Integer.parseInt(digito.toString());
-        }
+        sumaAlternada += Integer.parseInt(Character.toString(digitos[2]));
+        sumaAlternada -= Integer.parseInt(Character.toString(digitos[1]));
+        sumaAlternada += Integer.parseInt(Character.toString(digitos[0]));
         
         Character aux = sumaAlternada == 10 ? 'k' : sumaAlternada.toString().charAt(0);
         
@@ -89,7 +102,7 @@ public class RegistroRequest {
             return false;
         }
         
-        if(!Character.isDigit(dv) || Character.toLowerCase(dv) != 'k'){
+        if(!Character.isDigit(dv) && Character.toLowerCase(dv) != 'k'){
             forwardError("El digito verificador debe ser un numero entre 1-9 o una 'k'!");
             return false;
         }
@@ -97,12 +110,39 @@ public class RegistroRequest {
         return true;
     }
     
-    private boolean validarCorreo() {
-        return false;
+    private boolean validarCorreo() throws ServletException, IOException {
+        try {
+            InternetAddress mail = new InternetAddress(parameters.get("correo"));
+        } catch (AddressException ex) {
+            forwardError("El correo ingresado es invalido!");
+            return false;
+        }
+        return true;
     }
     
-    private boolean validarExistenciaUser() {
-        return false;
+    private boolean validarExistenciaUser() throws ServletException, IOException {
+        List<Usuario> usuarios = usuarioFacade.findAll();
+        
+        for(Usuario user : usuarios) {
+            if(user.getUserName().equals(parameters.get("user"))){
+                forwardError("Este nombre de usuario ya se encuentra registrado");
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean validarPassword() throws ServletException, IOException {
+        String pass1 = parameters.get("pass");
+        String pass2 = parameters.get("repetirPass");
+        
+        if(!pass1.equals(pass2)){
+            forwardError("Las contraseñas no son identicas!");
+            return false;
+        }
+        
+        return true;
     }
     
     private boolean validarCadenasVacias() throws ServletException, IOException {
